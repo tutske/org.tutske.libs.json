4156e528258c93cbee9d6478a3dd14ece5a9db17
Groot Sarchy <groot.sarchy@gmail.com>
2020-05-14 15:14:51 +0200

    feat: Add convenience mapper configuration
    ``ObjectMapper``s don't work well with more recent java types, there is
    a standard library to add the serialization and deserialization. However
    these are not present when calling the default constructor. The same
    happens with classes from ``java.time``. In addition instants are
    serialized as numbers (seconds since epoch) while they are probably
    better serialized as iso strings.
    
    Therefore we have added a method that will return a preconfigured
    instance: ``Mappers.createBaseMapper ()``.
    
    A second issues is that adding a custom serializer involves a lot of
    boilerplate code
    
    .. code-block::
    
        ObjectMapper mapper = ...;
        SimpleModel module = new SimpleModel ();
        module.addSerializer (Custom.class, new StdSerializer<Custom> (clazz) {
            @Override
            public void serialize (Custom value, JsonGenerator gen, SerializerProvider provider)
            throws IOException {
                /* ... */
            }
        });
        mapper.registerModule (moduel);
    
    While the only pieces of real code there are the class we wish to
    serialize and the body of the inner class. This commit adds a functional
    interface that captures the essence of the serialization, and some
    methods to add configuration to a module and an object mapper
    
    .. code-block::
    
        ObjectMapper mapper = ...;
        Mappers.configure (mapper, Mappers.module (m -> {
        	Mappers.serialize (m, Custom.class, (value, gen, provider) -> {
    	    /* ... */
    	});
        });

---
+++ src/main/java/org/tutske/lib/json/Mappers.java
@@ -0,0 +1,75 @@
+package org.tutske.lib.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+
+public class Mappers {
+
+	static final ObjectMapper instance = mapper ();
+
+	public static ObjectMapper mapper () {
+		return new ObjectMapper ()
+			.disable (SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+			.registerModule (new Jdk8Module ())
+			.registerModule (new JavaTimeModule ())
+			.registerModule (module (m -> m.addSerializer (
+				JsonException.class,
+				new JsonException.ResponseExceptionSerializer ()
+			)))
+		;
+	}
+
+	public static ObjectMapper mapper (Module ... modules) {
+		return configure (mapper (), modules);
+	}
+
+	public static ObjectMapper mapper (Consumer<SimpleModule> ... configs) {
+		return configure (mapper (), configs);
+	}
+
+	public static SimpleModule module (Consumer<SimpleModule> ... configs) {
+		SimpleModule module = new SimpleModule ();
+		for (Consumer<SimpleModule> config : configs ) { config.accept (module); }
+		return module;
+	}
+
+	public static ObjectMapper configure (ObjectMapper mapper, Module ... modules) {
+		for ( Module module : modules ) { mapper.registerModule (module); }
+		return mapper;
+	}
+
+	public static ObjectMapper configure (ObjectMapper mapper, Consumer<SimpleModule> ... configs) {
+		for ( Consumer<SimpleModule> config : configs ) {
+			SimpleModule module = new SimpleModule ();
+			config.accept (module);
+			mapper.registerModule (module);
+		}
+		return mapper;
+	}
+
+	public static <T> void serialize (SimpleModule module, Class<T> clazz, SerializerFn<T> serializer) {
+		module.addSerializer (clazz, new StdSerializer<T> (clazz) {
+			@Override public void serialize (T value, JsonGenerator gen, SerializerProvider provider)
+			throws IOException {
+				serializer.serialize (value, gen, provider);
+			}
+		});
+	}
+
+	@FunctionalInterface
+	public static interface SerializerFn<T> {
+		public void serialize (T value, JsonGenerator gen, SerializerProvider provider) throws IOException;
+	}
+
+}
---
+++ src/test/java/org/tutske/lib/json/MappersTest.java
@@ -0,0 +1,146 @@
+package org.tutske.lib.json;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+
+public class MappersTest {
+
+	@Test
+	public void it_should_add_custom_serializers_to_a_mapper () {
+		ObjectMapper mapper = Mappers.configure (new ObjectMapper (), module -> {
+			Mappers.serialize (module, CustomEntity.class, (value, gen, provider) -> {
+				gen.writeStartObject ();
+				gen.writeObjectField ("__type__", "CustomEntity");
+				gen.writeObjectField (String.valueOf (value.id), value.name);
+				gen.writeEndObject ();
+			});
+		});
+
+		JsonNode node = mapper.valueToTree (new CustomEntity (1, "John Doe"));
+		assertThat (node.path ("__type__").asText (), is ("CustomEntity"));
+	}
+
+	@Test
+	public void it_should_add_custom_serializers_to_a_default_mapper () {
+		ObjectMapper mapper = Mappers.mapper (module -> {
+			Mappers.serialize (module, CustomEntity.class, (value, gen, provider) -> {
+				gen.writeStartObject ();
+				gen.writeObjectField ("__type__", "CustomEntity");
+				gen.writeObjectField (String.valueOf (value.id), value.name);
+				gen.writeEndObject ();
+			});
+		});
+
+		JsonNode node = mapper.valueToTree (new CustomEntity (1, "John Doe"));
+		assertThat (node.path ("__type__").asText (), is ("CustomEntity"));
+	}
+
+
+	@Test
+	public void it_should_allow_configuring_with_modules () {
+		ObjectMapper mapper = Mappers.configure (new ObjectMapper (),
+			new Jdk8Module (), new JavaTimeModule ()
+		);
+
+		Instant now = Instant.now ();
+		JsonNode node = mapper.valueToTree (now);
+
+		assertThat (node.isNumber (), is (true));
+		assertThat (node.asLong (), is (now.getEpochSecond ()));
+	}
+
+	@Test
+	public void it_should_create_a_mapper_with_modules () {
+		ObjectMapper mapper = Mappers.mapper (new Jdk8Module (), new JavaTimeModule ());
+
+		Instant now = Instant.now ();
+		JsonNode node = mapper.valueToTree (now);
+
+		assertThat (node.isTextual (), is (true));
+		assertThat (node.asText (), containsString (DateTimeFormatter.ISO_INSTANT.format (now)));
+	}
+
+	@Test
+	public void it_should_create_a_simple_module_with_serializers () {
+		SimpleModule module = Mappers.module (m -> {
+			Mappers.serialize (m, CustomEntity.class, (value, gen, provider) -> {
+				gen.writeStartObject ();
+				gen.writeObjectField ("__type__", "CustomEntity");
+				gen.writeObjectField (String.valueOf (value.id), value.name);
+				gen.writeEndObject ();
+			});
+		});
+
+		ObjectMapper mapper = Mappers.mapper (module);
+
+		JsonNode node = mapper.valueToTree (new CustomEntity (1, "John Doe"));
+		assertThat (node.path ("__type__").asText (), is ("CustomEntity"));
+	}
+
+	@Test
+	public void it_should_create_a_base_mapper_that_uses_handles_optionals () {
+		ObjectMapper mapper = Mappers.mapper ();
+		JsonNode node = mapper.valueToTree (Json.objectNode (
+			"present", Optional.of ("value"),
+			"absent", Optional.empty ()
+		));
+
+		assertThat (node.path ("present").isTextual (), is (true));
+		assertThat (node.path ("present").asText (), is ("value"));
+		assertThat (node.path ("absent").isNull (), is (true));
+	}
+
+	@Test
+	public void it_should_create_a_base_mapper_that_uses_iso_date_time_formats () {
+		ObjectMapper mapper = Mappers.mapper ();
+
+		JsonNode node = mapper.valueToTree (Json.objectNode (
+			"iat", LocalDateTime.of (2020, 11, 23, 16, 5).atZone (ZoneId.of ("UTC")).toInstant ()
+		));
+
+		assertThat (node.path ("iat").isTextual (), is (true));
+		assertThat (node.path ("iat").asText (), containsString ("2020-11-23T16:05"));
+	}
+
+	@Test
+	public void it_should_configure_modules_with_methods_that_return_something () {
+		ObjectMapper mapper = Mappers.mapper (Mappers.module (this::addSerializers));
+		JsonNode node = mapper.valueToTree (new CustomEntity (1, "John Doe"));
+		assertThat (node.path ("__type__").asText (), is ("CustomEntity"));
+	}
+
+	public SimpleModule addSerializers (SimpleModule module) {
+		Mappers.serialize (module, CustomEntity.class, (value, gen, provider) -> {
+			gen.writeStartObject ();
+			gen.writeObjectField ("__type__", "CustomEntity");
+			gen.writeObjectField (String.valueOf (value.id), value.name);
+			gen.writeEndObject ();
+		});
+		return module;
+	}
+
+	public static class CustomEntity {
+		public final long id;
+		public final String name;
+
+		public CustomEntity (long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+	}
+
+}
