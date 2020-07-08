2fb7d86f920a0a9047a6e7f5c080a62709451134
Groot Sarchy <groot.sarchy@gmail.com>
2020-07-08 07:57:36 +0200

    feat: Make the jackson modules transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module org.tutske.libs.json {
 	exports org.tutske.lib.json;
 
 	/* named automatic modules */
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.databind;
-	requires com.fasterxml.jackson.datatype.jdk8;
-	requires com.fasterxml.jackson.datatype.jsr310;
+	requires transitive com.fasterxml.jackson.core;
+	requires transitive com.fasterxml.jackson.databind;
+	requires transitive com.fasterxml.jackson.datatype.jdk8;
+	requires transitive com.fasterxml.jackson.datatype.jsr310;
 
 }
